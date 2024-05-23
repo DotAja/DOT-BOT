402d3cb3de1746314992e1703dcb3a88b37d7d88
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-24 01:13:46 +0700

    created hendi kusnandi

--- kontol.py
+++ kontol.py
@@ -1,45 +1,53 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import requests
+import time
 
+proxy_url = "https://raw.githubusercontent.com/DotAja/DOT-BOT/main/memek.txt"
+credentials_url = "https://raw.githubusercontent.com/DotAja/DOT-BOT/main/kontol.txt"
 
-credentials = [
-    {'email': 'renda89@dmxs8.com', 'password': 'renda89@dmxs8.com'},
-    {'email': 'chrissilvey@dmxs8.com', 'password': 'chrissilvey@dmxs8.com'},
-    {'email': 'metlina69@dmxs8.com', 'password': 'metlina69@dmxs8.com'},
-    {'email': 'smudgeman@dmxs8.com', 'password': 'smudgeman@dmxs8.com'},
-    {'email': 'ibarrafd@dmxs8.com', 'password': 'ibarrafd@dmxs8.com'}
-]
+proxies = requests.get(proxy_url).text.strip().split("\n")
+emails = requests.get(credentials_url).text.strip().split("\n")
 
+def create_driver(proxy):
+    options = webdriver.ChromeOptions()
 
-def create_driver():
-    driver = webdriver.Chrome()
+    if proxy.startswith('socks'):
+        options.add_argument('--proxy-server=socks5://%s' % proxy)
+    else:
+        options.add_argument('--proxy-server=http://%s' % proxy)
+
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
+    driver.get('https://ide.goorm.io/my/dashboard/')
     return driver
 
+for i, email in enumerate(emails):
+    proxy_index = i % len(proxies)
+    proxy = proxies[proxy_index]
 
-for cred in credentials:
+    driver = create_driver(proxy)
 
-    driver = create_driver()
+    email_input = driver.find_element(By.ID, 'emailInput')
+    password_input = driver.find_element(By.ID, 'passwordInput')
 
+    email_input.clear()
+    password_input.clear()
 
-    driver.get('https://ide.goorm.io/my/dashboard/')
-
-
-    email = driver.find_element(By.ID, 'emailInput')
-    email.clear()
-    email.send_keys(cred['email'])
-
-
-    password = driver.find_element(By.ID, 'passwordInput')
-    password.clear()
-    password.send_keys(cred['password'])
-
+    email_input.send_keys(email)
+    password_input.send_keys(email)
 
     login_button = driver.find_element(By.XPATH, "//span[contains(@class, 'Tmopq5aSmHQftHmuZSsAd')]//span[contains(text(), 'Login')]")
     login_button.click()
 
+    time.sleep(30)
 
-    driver.implicitly_wait(5)
+    close_button = driver.find_element(By.XPATH, "//div[@class='modal-content']//button[@aria-label='Close']")
+    close_button.click()
 
+    time.sleep(2)
+    
+    create_resource_button = driver.find_element(By.ID, "hibernate_cardRun_dInv3CY5igiBpyMoV2A")
+    create_resource_button.click()
 
     driver.quit()
